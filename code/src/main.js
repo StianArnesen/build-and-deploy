0685e6405135b6b4e7d32296b140677d7a01343a
Stian Arnesen <stian_arnesen@outlook.com>
2025-11-11 19:17:09 +0100

    Fixed unused variable

--- code/src/main.js
+++ code/src/main.js
@@ -3,8 +3,6 @@ import {  getSecondsLeftOfYear, getTimeString } from "./clock";
 const timeContainer = document.getElementById("time");
 const timeLeft = document.getElementById("seconds-left");
 
-const unusedVariable = 3;
-
 const render = () => {
   const now = new Date();
   timeLeft.innerText = getSecondsLeftOfYear(now);
