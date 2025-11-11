eb0e8d08d77ec197d0c7fe76c391f488b19a1200
Stian Arnesen <stian_arnesen@outlook.com>
2025-11-11 19:15:18 +0100

    * Added unused variable. * new YML

--- code/src/main.js
+++ code/src/main.js
@@ -3,6 +3,8 @@ import {  getSecondsLeftOfYear, getTimeString } from "./clock";
 const timeContainer = document.getElementById("time");
 const timeLeft = document.getElementById("seconds-left");
 
+const unusedVariable = 3;
+
 const render = () => {
   const now = new Date();
   timeLeft.innerText = getSecondsLeftOfYear(now);
